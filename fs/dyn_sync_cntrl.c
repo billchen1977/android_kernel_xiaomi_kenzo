395b47dcbec43e7cc722142543025ae92f7c8a3f
Varun Chitre <varun.chitre15@gmail.com>
2015-02-15 04:36:31 -0500

    Forward port Dynamic Fsync to Linux 3.10
    *. Replace early_suspend with fb notify
    *. Fixup fsync mechanism to match the new api
    
    Change-Id: I030bc1b20eb103d413f89dde61c46ba979809107

--- fs/dyn_sync_cntrl.c
+++ fs/dyn_sync_cntrl.c
@@ -18,22 +18,24 @@
 #include <linux/module.h>
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
-#include <linux/earlysuspend.h>
 #include <linux/mutex.h>
 #include <linux/notifier.h>
 #include <linux/reboot.h>
 #include <linux/writeback.h>
 
-#define DYN_FSYNC_VERSION_MAJOR 1
-#define DYN_FSYNC_VERSION_MINOR 2
+#if defined(CONFIG_FB)
+#include <linux/notifier.h>
+#include <linux/fb.h>
+#elif defined(CONFIG_HAS_EARLYSUSPEND)
+#include <linux/earlysuspend.h>
+#endif
 
-/*
- * fsync_mutex protects dyn_fsync_active during early suspend / late resume
- * transitions
- */
+#define DYN_FSYNC_VERSION_MAJOR 2
+#define DYN_FSYNC_VERSION_MINOR 0
+
 static DEFINE_MUTEX(fsync_mutex);
 
-bool early_suspend_active __read_mostly = false;
+bool power_suspend_active __read_mostly = false;
 bool dyn_fsync_active __read_mostly = true;
 
 static ssize_t dyn_fsync_active_show(struct kobject *kobj,
@@ -67,15 +69,15 @@ static ssize_t dyn_fsync_active_store(struct kobject *kobj,
 static ssize_t dyn_fsync_version_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
-	return sprintf(buf, "version: %u.%u by faux123\n",
+	return sprintf(buf, "version: %u.%u\n",
 		DYN_FSYNC_VERSION_MAJOR,
 		DYN_FSYNC_VERSION_MINOR);
 }
 
-static ssize_t dyn_fsync_earlysuspend_show(struct kobject *kobj,
+static ssize_t dyn_fsync_powersuspend_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
-	return sprintf(buf, "early suspend active: %u\n", early_suspend_active);
+	return sprintf(buf, "power suspend active: %u\n", power_suspend_active);
 }
 
 static struct kobj_attribute dyn_fsync_active_attribute = 
@@ -86,14 +88,14 @@ static struct kobj_attribute dyn_fsync_active_attribute =
 static struct kobj_attribute dyn_fsync_version_attribute = 
 	__ATTR(Dyn_fsync_version, 0444, dyn_fsync_version_show, NULL);
 
-static struct kobj_attribute dyn_fsync_earlysuspend_attribute = 
-	__ATTR(Dyn_fsync_earlysuspend, 0444, dyn_fsync_earlysuspend_show, NULL);
+static struct kobj_attribute dyn_fsync_powersuspend_attribute = 
+	__ATTR(Dyn_fsync_powersuspend, 0444, dyn_fsync_powersuspend_show, NULL);
 
 static struct attribute *dyn_fsync_active_attrs[] =
 	{
 		&dyn_fsync_active_attribute.attr,
 		&dyn_fsync_version_attribute.attr,
-		&dyn_fsync_earlysuspend_attribute.attr,
+		&dyn_fsync_powersuspend_attribute.attr,
 		NULL,
 	};
 
@@ -112,11 +114,44 @@ static void dyn_fsync_force_flush(void)
 	sync_filesystems(1);
 }
 
+#if defined(CONFIG_FB)
+static int dyn_fsync_fb_event(struct notifier_block *self,
+				 unsigned long event, void *data)
+{
+	struct fb_event *evdata = data;
+	int *blank;
+
+	if (evdata && evdata->data) {
+		blank = evdata->data;
+		if (event == FB_EVENT_BLANK) {
+			if (*blank == FB_BLANK_POWERDOWN) {
+				mutex_lock(&fsync_mutex);
+				if (dyn_fsync_active) {
+					power_suspend_active = true;
+					dyn_fsync_force_flush();
+				}
+				mutex_unlock(&fsync_mutex);
+			} else if (*blank == FB_BLANK_UNBLANK) {
+				mutex_lock(&fsync_mutex);
+				power_suspend_active = false;
+				mutex_unlock(&fsync_mutex);
+			}
+		}
+	}
+
+	return 0;
+}
+
+static struct notifier_block dyn_fsync_fb_block =
+	{
+		.notifier_call = dyn_fsync_fb_event,
+	};
+#elif defined(CONFIG_HAS_EARLYSUSPEND)
 static void dyn_fsync_early_suspend(struct early_suspend *h)
 {
 	mutex_lock(&fsync_mutex);
 	if (dyn_fsync_active) {
-		early_suspend_active = true;
+		power_suspend_active = true;
 		dyn_fsync_force_flush();
 	}
 	mutex_unlock(&fsync_mutex);
@@ -125,21 +160,22 @@ static void dyn_fsync_early_suspend(struct early_suspend *h)
 static void dyn_fsync_late_resume(struct early_suspend *h)
 {
 	mutex_lock(&fsync_mutex);
-	early_suspend_active = false;
+	power_suspend_active = false;
 	mutex_unlock(&fsync_mutex);
 }
 
-static struct early_suspend dyn_fsync_early_suspend_handler = 
+static struct early_suspend dyn_fsync_power_suspend_handler = 
 	{
-		.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN,
+		.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN, 
 		.suspend = dyn_fsync_early_suspend,
 		.resume = dyn_fsync_late_resume,
 	};
+#endif
 
 static int dyn_fsync_panic_event(struct notifier_block *this,
 		unsigned long event, void *ptr)
 {
-	early_suspend_active = true;
+	power_suspend_active = true;
 	dyn_fsync_force_flush();
 	//pr_warn("dyn fsync: panic: force flush!\n");
 
@@ -155,7 +191,7 @@ static int dyn_fsync_notify_sys(struct notifier_block *this, unsigned long code,
 				void *unused)
 {
 	if (code == SYS_DOWN || code == SYS_HALT) {
-		early_suspend_active = true;
+		power_suspend_active = true;
 		dyn_fsync_force_flush();
 		//pr_warn("dyn fsync: reboot: force flush!\n");
 	}
@@ -170,7 +206,11 @@ static int dyn_fsync_init(void)
 {
 	int sysfs_result;
 
-	register_early_suspend(&dyn_fsync_early_suspend_handler);
+#if defined(CONFIG_FB)
+	fb_register_client(&dyn_fsync_fb_block);
+#elif defined(CONFIG_HAS_EARLYSUSPEND)
+	register_early_suspend(&dyn_fsync_power_suspend_handler);
+#endif
 	register_reboot_notifier(&dyn_fsync_notifier);
 	atomic_notifier_chain_register(&panic_notifier_list,
 		&dyn_fsync_panic_block);
@@ -193,7 +233,11 @@ static int dyn_fsync_init(void)
 
 static void dyn_fsync_exit(void)
 {
-	unregister_early_suspend(&dyn_fsync_early_suspend_handler);
+#if defined(CONFIG_FB)
+	fb_unregister_client(&dyn_fsync_fb_block);
+#elif defined(CONFIG_HAS_EARLYSUSPEND)
+	unregister_early_suspend(&dyn_fsync_power_suspend_handler);
+#endif
 	unregister_reboot_notifier(&dyn_fsync_notifier);
 	atomic_notifier_chain_unregister(&panic_notifier_list,
 		&dyn_fsync_panic_block);
@@ -201,6 +245,7 @@ static void dyn_fsync_exit(void)
 	if (dyn_fsync_kobj != NULL)
 		kobject_put(dyn_fsync_kobj);
 }
-
+MODULE_AUTHOR("Paul Reioux <reioux@gmail.com>");
+MODULE_AUTHOR("Varun Chitre <varun.chitre15@gmail.com");
 module_init(dyn_fsync_init);
 module_exit(dyn_fsync_exit);
